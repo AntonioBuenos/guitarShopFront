1e553804ec2c2c0fd91d6e8b76dc47e297cabcdd
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-04-06 22:11:25 +0300

    Landing page component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LangingPageComponent } from './components/langing-page/langing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LangingPageComponent
   ],
   imports: [
     BrowserModule,
