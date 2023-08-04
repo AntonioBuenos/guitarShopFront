6528e8fc763542287932af56f7503421ef557967
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-08-04 23:15:46 +0300

    Landing page restructured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LangingPageComponent } from './components/langing-page/langing-page.component';
+import { LangingPageComponent } from './controllers/langing-page/langing-page.component';
 
 @NgModule({
   declarations: [
