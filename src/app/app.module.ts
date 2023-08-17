b9f5729ef1fe2502994166f20449f50fdf64b7b5
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-08-17 22:07:28 +0300

    Main menu component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LangingPageComponent } from './controllers/langing-page/langing-page.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LangingPageComponent
+    LangingPageComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
