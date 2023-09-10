809f71f840f593e811d089c2e67da54fe8364595
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-09-10 21:02:41 +0300

    config fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './controllers/landing-page/landing-page.component';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { UsersComponent } from './controllers/users/users.component';
-import { GuitarsComponent } from './controllers/guitars/guitars.component';
-import { BrandsComponent } from './controllers/brands/brands.component';
-import { InstockComponent } from './controllers/instock/instock.component';
-import { PageNotFoundComponent } from './controllers/page-not-found/page-not-found.component';
-import { UsersSearchComponent } from './controllers/users-search/users-search.component';
-import { BrandsSearchComponent } from './controllers/brands-search/brands-search.component';
-import { GuitarsSearchComponent } from './controllers/guitars-search/guitars-search.component';
-import { InstockSearchComponent } from './controllers/instock-search/instock-search.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LandingPageComponent} from './controllers/landing-page/landing-page.component';
+import {MainMenuComponent} from './components/main-menu/main-menu.component';
 import {TranslateModule} from "@ngx-translate/core";
+import {NgxPermissionsModule, NgxPermissionsService, NgxRolesService} from "ngx-permissions";
 
 @NgModule({
   declarations: [
@@ -25,9 +17,13 @@ import {TranslateModule} from "@ngx-translate/core";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxPermissionsModule.forRoot(),
     TranslateModule
   ],
-  providers: [],
+  providers: [
+    NgxPermissionsService,
+    NgxRolesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
