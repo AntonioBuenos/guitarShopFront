3c54fe1c93efc41136ca00627a65f8d4d958af22
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-09-11 22:27:31 +0300

    brands-routing.module.ts added

--- src/app/controllers/brands/brands.module.ts
+++ src/app/controllers/brands/brands.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {UsersRoutingModule} from "../users/users-routing.module";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UsersRoutingModule
   ]
 })
 export class BrandsModule { }
